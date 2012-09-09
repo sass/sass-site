e355ce7779183532896cdf017651549764d877ce
Jina Bolton <jina@sushiandrobots.com>
2012-09-09 16:37:46 -0700

    - Replace Octopress files with Middleman, per Basecamp discussion.
    - Add HTML5 Boilerplate as a starting base for prototyping out the content.
    
    - Convert files to Haml & Sass.
    
    - Begin homepage content.
    
    - Add Susy.

--- Gemfile
+++ Gemfile
@@ -1,18 +1,5 @@
-source "http://rubygems.org"
+source :rubygems
 
-group :development do
-  gem 'rake'
-  gem 'rack'
-  gem 'jekyll'
-  gem 'rdiscount'
-  gem 'pygments.rb'
-  gem 'RedCloth'
-  gem 'haml', '>= 3.1'
-  gem 'compass', '>= 0.11'
-  gem 'rubypants'
-  gem 'rb-fsevent'
-  gem 'stringex'
-  gem 'liquid', '2.2.2'
-end
-
-gem 'sinatra', '1.2.6'
+gem "middleman", "~>3.0.2"
+gem "compass"
+gem "susy"
--- config.rb
+++ config.rb
@@ -1,16 +1,77 @@
-# Require any additional compass plugins here.
-project_type = :stand_alone
+###
+# Compass
+###
 
-# Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+# Susy grids in Compass
+# First: gem install susy --pre
+require "susy"
 
-# Local development paths
-sass_dir = "sass"
-images_dir = "source/images"
-fonts_dir = "source/fonts"
+# Change Compass configuration
+compass_config do |config|
+  config.output_style = :compact
+end
 
-line_comments = false
-output_style = :compressed
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
