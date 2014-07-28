352f71fd1afe5a851d6a161d5b8d6105c86c060c
Jina Bolton <jina@sushiandrobots.com>
2014-07-27 18:58:17 -0700

    Update site to remove IE conditionals

--- helpers/sass_helpers.rb
+++ helpers/sass_helpers.rb
@@ -1,10 +1,12 @@
+require "pathname"
+
 module SassHelpers
   def page_title
     title = "Sass: "
     if data.page.title
       title << data.page.title
     else
-      title << "CSS with Super Powers"
+      title << t("index.sub_title")
     end
     title
   end
@@ -14,7 +16,32 @@ module SassHelpers
     if start_year == end_year
       start_year.to_s
     else
-      start_year.to_s + '-' + end_year.to_s
+      start_year.to_s + '&ndash;' + end_year.to_s
     end
+  end
+
+  def pages_for_group(group_name)
+    group = data.nav.find do |g|
+      g.name == group_name
+    end
+
+    pages = []
+
+    return pages unless group
+
+    if group.directory
+      pages << sitemap.resources.select { |r|
+        r.path.match(%r{^#{group.directory}}) && !r.data.hidden
+      }.map do |r|
+        ::Middleman::Util.recursively_enhance({
+          :title => r.data.title,
+          :path  => r.url
+        })
+      end.sort_by { |p| p.title }
+    end
+
+    pages << group.pages if group.pages
+
+    pages.flatten
   end
 end
