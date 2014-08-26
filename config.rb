e0acbc6a287deb767a75f25867c78d9dc5f3dfac
Jina Bolton <jina@sushiandrobots.com>
2014-08-25 17:59:09 -0700

    Add community guidelines.

--- config.rb
+++ config.rb
@@ -15,14 +15,15 @@ set :css_dir,    'assets/css'
 set :js_dir,     'assets/js'
 set :images_dir, 'assets/img'
 
-page "/humans.txt",      :layout => false
-page "/sitemap.xml",     :layout => false
-page "/404.html",        :layout => :layout_2_column
-page "/about.html",      :layout => :layout_2_column
-page "/guide.html",      :layout => :layout_2_column
-page "/libsass.html",    :layout => :layout_2_column
-page "/sitemap.html",    :layout => :layout_2_column
-page "/documentation/*", :directory_index => false
+page "/humans.txt",                :layout => false
+page "/sitemap.xml",               :layout => false
+page "/404.html",                  :layout => :layout_2_column
+page "/about.html",                :layout => :layout_2_column
+page "/guide.html",                :layout => :layout_2_column
+page "/libsass.html",              :layout => :layout_2_column
+page "/sitemap.html",              :layout => :layout_2_column
+page "/community-guidelines.html", :layout => :layout_2_column
+page "/documentation/*",           :directory_index => false
 
 with_layout :styleguide do
   page "/styleguide/*"
