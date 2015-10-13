8aac006d61b4ed063f45d57b14da291f1b86eb59
jina <jina@sushiandrobots.com>
2015-10-13 16:55:29 -0700

    update gems, cleanup, and surface implementation guide better

--- config.rb
+++ config.rb
@@ -1,11 +1,25 @@
 require 'breakpoint'
-require 'builder'
 require 'middleman-syntax'
 require 'susy'
-require 'typogruby'
 
+activate :automatic_image_sizes
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions', 'Explorer >= 9']
+  config.remove   = false
+  config.cascade  = false
+  config.inline   = true
+  # config.ignore   = ['hacks.css']
+end
 activate :directory_indexes
 activate :syntax
+
+
+
+compass_config do |config|
+  config.output_style = :expanded
+end
+
+
 
 set :markdown, :fenced_code_blocks => true,
                :autolink => true,
@@ -14,6 +28,8 @@ set :markdown_engine, :redcarpet
 set :css_dir,    'assets/css'
 set :js_dir,     'assets/js'
 set :images_dir, 'assets/img'
+
+
 
 page "/humans.txt",                :layout => false
 page "/sitemap.xml",               :layout => false
@@ -33,10 +49,6 @@ end
 
 configure :development do
   activate :livereload
-
-  compass_config do |config|
-    config.output_style = :expanded
-  end
 end
 
 
