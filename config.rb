ffbafff55e90d7f31522c873bf733c490acc19db
jina <jina@sushiandrobots.com>
2015-10-13 17:02:12 -0700

    syntax fixes

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'susy'
 
 activate :automatic_image_sizes
 activate :autoprefixer do |config|
-  config.browsers = ['last 2 versions', 'Explorer >= 9']
+  config.browsers = ["last 2 versions", "Explorer >= 9"]
   config.remove   = false
   config.cascade  = false
   config.inline   = true
@@ -13,13 +13,9 @@ end
 activate :directory_indexes
 activate :syntax
 
-
-
 compass_config do |config|
   config.output_style = :expanded
 end
-
-
 
 set :markdown, :fenced_code_blocks => true,
                :autolink => true,
@@ -28,8 +24,6 @@ set :markdown_engine, :redcarpet
 set :css_dir,    'assets/css'
 set :js_dir,     'assets/js'
 set :images_dir, 'assets/img'
-
-
 
 page "/humans.txt",                :layout => false
 page "/sitemap.xml",               :layout => false
@@ -45,13 +39,9 @@ with_layout :styleguide do
   page "/styleguide/*"
 end
 
-
-
 configure :development do
   activate :livereload
 end
-
-
 
 configure :build do
   activate :asset_hash
