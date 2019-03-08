681d63c698e3bc2746f41be3cd20b0b2dd7dfed9
Natalie Weizenbaum <nweiz@google.com>
2019-03-08 15:13:37 -0800

    Follow the style guide for implementation notes

--- source/assets/js/sass.js
+++ source/assets/js/sass.js
@@ -70,3 +70,20 @@ $(function() {
     })
   ;
 });
+
+$(function() {
+  $(".impl-status").each(function() {
+    var statusBar = $(this);
+    var expandLink = $(this).find("a");
+    if (expandLink == null) return;
+
+    var details = $(this).next();
+    if (!details.hasClass("sl-c-callout")) return;
+
+    details.hide();
+    expandLink.click(function() {
+      details.toggle();
+      expandLink.text(expandLink.text() == "▶" ? "▼" : "▶");
+    });
+  });
+});
