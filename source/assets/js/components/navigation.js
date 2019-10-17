f80b43fcc1728e075ecf764c4fd6e22ec594d90e
Adekunle Oduye <adekunleoduye@gmail.com>
2019-10-16 21:38:52 -0400

    [hotfix-387] Fix broken toggle

--- source/assets/js/components/navigation.js
+++ source/assets/js/components/navigation.js
@@ -6,7 +6,7 @@
 
   // Added sticky class when window top is great than nav top
   function stickyNav() {
-    if ($(window).scrollTop() >= sticky.top) {
+    if ( nav.length > 0 && $(window).scrollTop() >= sticky.top) {
       $(".sl-l-medium-holy-grail__body").addClass("sl-js-nav--is-sticky");
     } else {
       $(".sl-l-medium-holy-grail__body").removeClass("sl-js-nav--is-sticky");
