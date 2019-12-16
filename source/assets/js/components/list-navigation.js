dcc6a4789e871a4f53e076e21e4a8bf483952a29
tatianamac <tatiana.t.mac@gmail.com>
2019-12-16 14:12:43 -0800

    🎨  Background color

--- source/assets/js/components/list-navigation.js
+++ source/assets/js/components/list-navigation.js
@@ -2,7 +2,6 @@ $(function() {
   $(".sl-c-list-navigation-wrapper--collapsible li > ul")
       .parent()
       .children("a")
-      // .removeAttr("href") Remove attr was causing keyboard focus skips
       .click(function() {
         $(this).toggleClass("open");
         return false;
