31d1bf5ae8ceda3557bd650ff3abbaa32199b3f7
tatianamac <tatiana.t.mac@gmail.com>
2019-12-18 15:43:16 -0800

    Revert "🦐  Fixed smol edits from Jina"
    This reverts commit be52b5d79b6721ccd68ea9f800a5863aae12a13e.

--- config.rb
+++ config.rb
@@ -121,7 +121,7 @@ after_render do |content, path, locs|
   content.gsub(%r{^<(h[0-6])(.*?)</\1>}m) do |header_text|
     header = Nokogiri::HTML::DocumentFragment.parse(header_text).children.first
     id = header.attr(:id)
-    header.children.before("<a class='anchor' href='##{id}'><span class='visuallyhidden'>#{header.text} permalink</span></a>") if id 
+    header.children.before("<a class='anchor' href='##{id}'><span class='visuallyhidden'>#{header.text} permalink</a>") if id 
     header.to_html
   end
 end
