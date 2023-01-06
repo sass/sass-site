7e2f28ad2e1eaa42b06e306865e19563b3621622
Jonny Gerig Meyer <jonny@oddbird.net>
2023-01-06 15:35:17 -0500

    Basic 11ty layout/include.

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,10 +1,13 @@
 'use strict';
 
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
   // settings
   return {
     dir: {
       input: 'source',
+      includes: '_includes',
+      layouts: '_layouts'
     },
   };
 };
---
+++ source/index.md
@@ -0,0 +1,6 @@
+---
+title: Test Title
+layout: base
+---
+
+🚧 👷 🚧
