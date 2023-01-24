5b27a22fed948c852e571c320db3dedc02207294
Jonny Gerig Meyer <jonny@oddbird.net>
2023-01-24 17:51:07 -0500

    Add contents for homepage

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,5 +1,9 @@
 'use strict';
 
+const yaml = require('js-yaml');
+const markdown = require('markdown-it');
+const typogrify = require('typogr');
+
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('source/assets/dist');
@@ -13,6 +17,31 @@ module.exports = (eleventyConfig) => {
     jsTruthy: true,
   });
   eleventyConfig.setUseGitIgnore(false);
+
+  const mdown = markdown({
+    html: true,
+    breaks: false,
+    typographer: true,
+  }).disable('code');
+
+  eleventyConfig.setLibrary('md', mdown);
+  eleventyConfig.addDataExtension('yaml', yaml.load);
+
+  eleventyConfig.addPairedLiquidShortcode('markdown', (content) =>
+    typogrify.typogrify(mdown.render(content)),
+  );
+
+  eleventyConfig.addPairedLiquidShortcode('markdownInline', (content) =>
+    typogrify.typogrify(mdown.renderInline(content)),
+  );
+
+  eleventyConfig.addLiquidFilter('typogr', (content) =>
+    typogrify.typogrify(content),
+  );
+
+  eleventyConfig.addLiquidFilter('isTypedoc', (page) =>
+    page.url.startsWith('/documentation/js-api/'),
+  );
 
   // settings
   return {
