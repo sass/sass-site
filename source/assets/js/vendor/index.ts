7fa2ff0e1fb528b25b99074731d1a8b2dc88813d
Jonny Gerig Meyer <jonny@oddbird.net>
2023-02-17 14:58:17 -0500

    Add more jQuery UI deps

--- source/assets/js/vendor/index.ts
+++ source/assets/js/vendor/index.ts
@@ -1,7 +1,12 @@
-import 'jquery';
-import 'jquery-ui';
+// import jQuery UI "Tabs" widget and dependencies
+// https://jqueryui.com/tabs/
+import 'jquery-ui/ui/version';
+import 'jquery-ui/ui/widget';
+import 'jquery-ui/ui/keycode';
 import 'jquery-ui/ui/unique-id';
+import 'jquery-ui/ui/safe-active-element';
 import 'jquery-ui/ui/widgets/tabs';
+
 import './jquery-smooth-scroll.min';
 import './modernizr.custom.min';
 import './html5-boilerplate/plugins';
