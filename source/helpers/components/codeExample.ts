43d62b6bef32a26a1954408bf72e0e14e99c23fc
Sana Javed <sanajaved7@gmail.com>
2023-05-22 17:15:52 +0200

    Updating codeExample logic to account for when there is no CSS in the code examples

--- source/helpers/components/codeExample.ts
+++ source/helpers/components/codeExample.ts
@@ -263,9 +263,9 @@ const getCanSplit = (
   const exampleSourceLengths = [...scssExamples, ...sassExamples].flatMap(
     (source) => source.split('\n').map((line) => line.length),
   );
-  const cssSourceLengths = cssExamples.flatMap((source) =>
+  const cssSourceLengths = cssExamples.length ? cssExamples.flatMap((source) =>
     source.split('\n').map((line) => line.length),
-  );
+  ) : [0];
 
   const maxSourceWidth = Math.max(...exampleSourceLengths);
   const maxCSSWidth = Math.max(...cssSourceLengths);
