8ae9bfd6b1f34e69a0c2d395b3956ed7cf146465
James Stuckey Weber <jamesnw@gmail.com>
2023-06-15 16:22:00 -0400

    Lint

--- source/assets/js/playground/theme.ts
+++ source/assets/js/playground/theme.ts
@@ -4,54 +4,52 @@ import { tags } from '@lezer/highlight';
 const playgroundHighlightStyle = HighlightStyle.define([
   {
     tag: [tags.special(tags.variableName), tags.tagName],
-    color: 'var(--sl-color--code-base)'
+    color: 'var(--sl-color--code-base)',
   },
-  { tag: tags.className,
+  {
+    tag: tags.className,
     color: 'var(--sl-color--code-cool)',
-    fontWeight: '600'
+    fontWeight: '600',
   },
-  { tag: tags.comment,
+  {
+    tag: tags.comment,
     color: 'var(--sl-color--code-muted)',
-    fontStyle: 'italic'
+    fontStyle: 'italic',
   },
-  { tag: tags.keyword,
+  {
+    tag: tags.keyword,
     color: 'var(--sl-color--code-bright-dark)',
-    fontWeight: '600'
+    fontWeight: '600',
   },
-  { tag: tags.controlKeyword,
+  {
+    tag: tags.controlKeyword,
     color: 'var(--sl-color--code-dark)',
-    fontWeight: '600'
+    fontWeight: '600',
   },
-  { tag: tags.definitionKeyword,
+  {
+    tag: tags.definitionKeyword,
     color: 'var(--sl-color--code-dark)',
-    fontWeight: '600'
+    fontWeight: '600',
   },
-  { tag: tags.moduleKeyword,
-    color: 'green'
-  },
-  { tag: tags.operator,
+  { tag: tags.moduleKeyword, color: 'green' },
+  {
+    tag: tags.operator,
     color: 'var(--sl-color--code-muted-dark)',
-    fontWeight: '600'
+    fontWeight: '600',
   },
-  { tag: tags.propertyName,
+  {
+    tag: tags.propertyName,
     color: 'var(--sl-color--code-warm)',
-    fontWeight: '600'
+    fontWeight: '600',
   },
-  { tag: tags.punctuation,
-    color: 'var(--sl-color--code-muted-dark)'
-  },
-  { tag: tags.string,
-    color: 'var(--sl-color--code-bright)'
-  },
-  { tag: tags.unit,
-    color: 'var(--sl-color--code-base)'
-  },
-  { tag: tags.atom,
-    color: 'var(--sl-color--code-base)'
-  },
-  { tag: tags.labelName,
+  { tag: tags.punctuation, color: 'var(--sl-color--code-muted-dark)' },
+  { tag: tags.string, color: 'var(--sl-color--code-bright)' },
+  { tag: tags.unit, color: 'var(--sl-color--code-base)' },
+  { tag: tags.atom, color: 'var(--sl-color--code-base)' },
+  {
+    tag: tags.labelName,
     color: 'var(--sl-color--code-dark)',
-    fontWeight: '600'
-  }
+    fontWeight: '600',
+  },
 ]);
 export { playgroundHighlightStyle };
