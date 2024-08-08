f5dbee6c3919c074dd089f581382941bbc90fe7b
James Stuckey Weber <james@oddbird.net>
2024-08-08 16:10:53 -0400

    Add more built in module values

--- source/assets/js/playground/autocomplete.ts
+++ source/assets/js/playground/autocomplete.ts
@@ -5,6 +5,7 @@ import {
 } from '@codemirror/autocomplete';
 import {sassCompletionSource} from '@codemirror/lang-sass';
 import {syntaxTree} from '@codemirror/language';
+import {EditorState} from '@codemirror/state';
 
 const atRuleKeywords = [
   'use',
@@ -43,17 +44,53 @@ function atRuleCompletion(context: CompletionContext): CompletionResult | null {
   };
 }
 
-const builtinModules = [
+type CompletionInfo = {
+  name: string;
+  description?: string;
+};
+
+type ModuleDefinition = CompletionInfo & {
+  functions?: CompletionInfo[];
+  variables?: CompletionInfo[];
+};
+
+const builtinModules: ModuleDefinition[] = [
   {
     name: 'color',
     description:
       'generates new colors based on existing ones, making it easy to build color themes',
   },
-  {name: 'list', description: 'lets you access and modify values in lists'},
+  {
+    name: 'list',
+    description: 'lets you access and modify values in lists',
+    functions: [
+      {name: 'length'},
+      {name: 'nth'},
+      {name: 'set-nth'},
+      {name: 'join'},
+      {name: 'append'},
+      {name: 'zip'},
+      {name: 'index'},
+      {name: 'is-bracketed'},
+      {name: 'separator'},
+      {name: 'slash'},
+    ],
+  },
   {
     name: 'map',
     description:
       'makes it possible to look up the value associated with a key in a map, and much more',
+    functions: [
+      {name: 'get'},
+      {name: 'set'},
+      {name: 'merge'},
+      {name: 'remove'},
+      {name: 'keys'},
+      {name: 'values'},
+      {name: 'has-key'},
+      {name: 'deep-merge'},
+      {name: 'deep-remove'},
+    ],
   },
   {
     name: 'math',
@@ -94,26 +131,71 @@ const builtinModules = [
       {name: 'random'},
     ],
   },
-  {name: 'meta', description: 'exposes the details of Sass’s inner workings'},
+  {
+    name: 'meta',
+    description: 'exposes the details of Sass’s inner workings',
+    functions: [
+      {name: 'apply'},
+      {name: 'load-css'},
+      {name: 'accepts-content'},
+      {name: 'calc-args'},
+      {name: 'call'},
+      {name: 'context-exists'},
+      {name: 'feature-exists'},
+      {name: 'function-exists'},
+      {name: 'mixin-exists'},
+      {name: 'variable-exists'},
+      {name: 'get-function'},
+      {name: 'get-mixin'},
+      {name: 'global-variable-exists'},
+      {name: 'keywords'},
+      {name: 'module-functions'},
+      {name: 'module-mixins'},
+      {name: 'module-variables'},
+      {name: 'type-of'},
+    ],
+  },
   {
     name: 'selector',
     description: 'provides access to Sass’s powerful selector engine',
+    functions: [
+      {name: 'isSuperselector'},
+      {name: 'simpleSelectors'},
+      {name: 'parse'},
+      {name: 'nest'},
+      {name: 'append'},
+      {name: 'extend'},
+      {name: 'replace'},
+      {name: 'unify'},
+    ],
   },
   {
     name: 'string',
     description: 'makes it easy to combine, search, or split apart strings',
+    functions: [
+      {name: 'unquote'},
+      {name: 'quote'},
+      {name: 'to-upper-case'},
+      {name: 'to-lower-case'},
+      {name: 'length'},
+      {name: 'insert'},
+      {name: 'index'},
+      {name: 'slice'},
+      {name: 'unique-id'},
+      {name: 'split'},
+    ],
   },
 ];
 
-const moduleNames = builtinModules.map(module => module.name);
+const moduleNames = builtinModules.map(mod => mod.name);
 const moduleNameRegExp = new RegExp(`(${moduleNames.join('|')}).\\$?(\\w)*`);
 
 const moduleCompletions = Object.freeze(
-  builtinModules.map(module => ({
-    label: `"sass:${module.name}"`,
+  builtinModules.map(mod => ({
+    label: `"sass:${mod.name}"`,
     // don't add extra quote on the end, as it likely is already there
-    apply: `"sass:${module.name}`,
-    info: module.description,
+    apply: `"sass:${mod.name}`,
+    info: mod.description,
     type: 'class',
   }))
 );
@@ -136,44 +218,71 @@ function builtinModulesCompletion(
 }
 
 const moduleVariableCompletions = Object.freeze(
-  builtinModules
-    .flatMap(module => {
-      return module.variables?.map(variable => ({
-        label: `${module.name}.${variable.name}`,
-        info: variable?.description,
-        type: 'variable',
-      }));
-    })
-    .filter(x => x !== undefined)
+  builtinModules.reduce(
+    (acc: {[k: string]: CompletionResult['options'] | []}, mod) => {
+      acc[mod.name] =
+        mod.variables?.map(variable => ({
+          label: `${mod.name}.${variable.name}`,
+          info: variable?.description,
+          type: 'variable',
+        })) || [];
+      return acc;
+    },
+    {}
+  )
 );
+
 const moduleFunctionsCompletions = Object.freeze(
-  builtinModules
-    .flatMap(module => {
-      return module.functions?.map(variable => ({
-        label: `${module.name}.${variable.name}`,
-        info: variable?.description,
-        type: 'method',
-        boost: 10,
-      }));
-    })
-    .filter(x => x !== undefined)
+  builtinModules.reduce(
+    (acc: {[k: string]: CompletionResult['options'] | []}, mod) => {
+      acc[mod.name] =
+        mod.functions?.map(variable => ({
+          label: `${mod.name}.${variable.name}`,
+          apply: `${mod.name}.${variable.name}(`,
+          info: variable?.description,
+          type: 'method',
+          boost: 10,
+        })) || [];
+      return acc;
+    },
+    {}
+  )
 );
+
+function includedBuiltinModules(state: EditorState) {
+  const text = state.doc.toString();
+  const modNames = builtinModules.map(mod => mod.name);
+  return modNames.filter(name => text.includes(`sass:${name}`));
+}
 
 function builtinModuleItemCompletion(
   context: CompletionContext
 ): CompletionResult | null {
-  // const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1);
-  // if (nodeBefore.parent?.type.name !== 'UseStatement') return null;
-
+  const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1);
+  if (
+    ![nodeBefore.type.name, nodeBefore.parent?.type.name].includes(
+      'NamespacedValue'
+    )
+  )
+    return null;
   const atRule = context.matchBefore(moduleNameRegExp);
-  console.log(atRule);
 
   if (!atRule) return null;
   if (atRule.from === atRule.to && !context.explicit) return null;
+
+  const includedModules = includedBuiltinModules(context.state);
+
+  const includedModFunctions = includedModules.flatMap(
+    mod => moduleFunctionsCompletions[mod]
+  );
+  const includedModVariables = includedModules.flatMap(
+    mod => moduleVariableCompletions[mod]
+  );
+
   return {
     from: atRule.from,
     to: atRule.to,
-    options: [...moduleVariableCompletions, ...moduleFunctionsCompletions],
+    options: [...includedModVariables, ...includedModFunctions],
   };
 }
 
