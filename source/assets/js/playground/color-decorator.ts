694d3e283df0b0783ed1be48e3279c1c14883e7f
James Stuckey Weber <james@oddbird.net>
2024-09-24 13:50:15 -0400

    Create decoarators only if valid

--- source/assets/js/playground/color-decorator.ts
+++ source/assets/js/playground/color-decorator.ts
@@ -15,18 +15,19 @@ const COLOR_NODE_TYPES = ['CallExpression', 'ColorLiteral', 'ValueName'];
 
 // The CallExpression types that may contain a color.
 const COLOR_FUNCTIONS = [
-  'oklch',
-  'oklab',
-  'rgb',
-  'rgba',
   'color',
   'hsl',
   'hsla',
   'lab',
   'lch',
+  'oklab',
+  'oklch',
+  'rgb',
+  'rgba',
 ];
 
-function colorPhrases(view: EditorView): DecorationSet {
+/** Parses content to add {@link ColorWidget} s after valid color declarations. */
+function getColorDecorations(view: EditorView): DecorationSet {
   const widgets: Range<Decoration>[] = [];
   for (const {from, to} of view.visibleRanges) {
     syntaxTree(view.state).iterate({
@@ -37,15 +38,24 @@ function colorPhrases(view: EditorView): DecorationSet {
           const colorString = view.state.doc.sliceString(node.from, node.to);
           if (
             node.name === 'CallExpression' &&
+            // Ensure the CallExpression is a color.
             !COLOR_FUNCTIONS.some(func => colorString.split('(')[0] === func)
           ) {
             return;
           }
-          const deco = Decoration.widget({
-            widget: new ColorWidget(colorString),
-            side: 1,
-          });
-          widgets.push(deco.range(node.to));
+          try {
+            const color = new Color(colorString);
+            const deco = Decoration.widget({
+              widget: new ColorWidget(color),
+              side: 1,
+            });
+            widgets.push(deco.range(node.to));
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          } catch (error) {
+            // Color constuctor throws when colorString does not represent a
+            // color.
+            return;
+          }
         }
       },
     });
@@ -53,33 +63,23 @@ function colorPhrases(view: EditorView): DecorationSet {
   return Decoration.set(widgets, true);
 }
 
+// A {Widget} showing a color and gamut info.
 class ColorWidget extends WidgetType {
-  colorObj: Color | undefined;
   inP3: boolean | undefined;
-  isValid: boolean;
-  constructor(readonly color: string) {
+  constructor(readonly color: Color) {
     super();
-    this.isValid = false;
-    try {
-      this.colorObj = new Color(color);
-      this.inP3 = this.colorObj.inGamut('p3');
-      this.isValid = true;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
-      this.isValid = false;
-    }
+    this.inP3 = this.color.inGamut('p3');
   }
 
   eq(other: ColorWidget): boolean {
     return other.color === this.color;
   }
 
-  toDOM(): HTMLSpanElement | HTMLDivElement {
-    if (!this.isValid) return document.createElement('span');
+  toDOM(): HTMLDivElement {
     const wrap = document.createElement('div');
     wrap.setAttribute('aria-hidden', 'true');
     wrap.className = 'cm-color-swatch';
-    wrap.style.setProperty('--color', this.color);
+    wrap.style.setProperty('--color', this.color.toString());
     if (this.inP3) wrap.setAttribute('in-gamut', 'in-gamut');
     else wrap.setAttribute('title', 'Outside of P3 gamut');
     const box = wrap.appendChild(document.createElement('div'));
@@ -88,11 +88,12 @@ class ColorWidget extends WidgetType {
   }
 }
 
+/** A {@link ViewPlugin} that shows colors inline with their declarations. */
 export const colorDecorator = ViewPlugin.fromClass(
   class {
     colors: DecorationSet;
     constructor(view: EditorView) {
-      this.colors = colorPhrases(view);
+      this.colors = getColorDecorations(view);
     }
 
     update(update: ViewUpdate): void {
@@ -101,7 +102,7 @@ export const colorDecorator = ViewPlugin.fromClass(
         update.viewportChanged ||
         syntaxTree(update.startState) !== syntaxTree(update.state)
       )
-        this.colors = colorPhrases(update.view);
+        this.colors = getColorDecorations(update.view);
     }
   },
   {
