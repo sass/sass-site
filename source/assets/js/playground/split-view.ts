cddf61dd58dbdb585ff412445bce4f55965acfac
James Stuckey Weber <james@oddbird.net>
2024-09-27 16:28:53 -0400

    Check layout on initial load, doc

--- source/assets/js/playground/split-view.ts
+++ source/assets/js/playground/split-view.ts
@@ -1,10 +1,17 @@
 import {SplitView} from '@spectrum-web-components/split-view';
 import '@spectrum-web-components/split-view/sp-split-view.js';
 
-export default function setup(): void {
+/**
+ * Make split views responsive and stack vertically on narrow screens.
+ */
+export default function setupSplitView(): void {
+  // 60em = --sl-breakpoint--large
   const mql = window.matchMedia('(max-width: 60em');
-  mql.addEventListener('change', event => {
-    const topSplit = document.querySelector('.sl-c-playground') as SplitView;
+  const topSplit = document.querySelector('.sl-c-playground') as SplitView;
+
+  function updateLayout(event: MediaQueryListEvent | MediaQueryList): void {
     if (topSplit) topSplit.vertical = event.matches;
-  });
+  }
+  updateLayout(mql);
+  mql.addEventListener('change', updateLayout);
 }
