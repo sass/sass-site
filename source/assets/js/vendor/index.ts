4b8b4a58f98f79870c7946670604a4ac28f87e99
Carlos <goodwine@google.com>
2026-05-05 22:39:49 +0000

    fixes tsc issues due to TS6
    - directly install @lezer/lr and style-mod to pull in types
    - remove 'jquery-ui/ui/safe-active-element' since it doesn't exist
    - add explicit rootDir in tsconfig.json
    - add explicit types file in tsonconfig.json

--- source/assets/js/vendor/index.ts
+++ source/assets/js/vendor/index.ts
@@ -4,7 +4,6 @@ import 'jquery-ui/ui/version';
 import 'jquery-ui/ui/widget';
 import 'jquery-ui/ui/keycode';
 import 'jquery-ui/ui/unique-id';
-import 'jquery-ui/ui/safe-active-element';
 import 'jquery-ui/ui/widgets/tabs';
 
 import './jquery-smooth-scroll.min';
